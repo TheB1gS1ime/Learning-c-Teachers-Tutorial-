fb478737f4536e7ecaec78dd0a37e91e9bfd2718
Josh Thom <dapperfrog01@gmail.com>
2025-09-19 22:02:44 +0100

    Lesson 04
    Final c# lesson on Mr Parr's youtube.

---
+++ Lesson 04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            swapit(a,b);
+        }
+
+        static void swapit(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
